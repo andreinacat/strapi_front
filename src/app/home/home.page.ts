2959f534aafacafc1a78acacc5e1de7476b742a1
andreinacat <andrea.verdugomunoz@gmail.com>
2021-10-01 00:19:43 -0300

    slides_tabs_update_1oct

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChild} from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +11,75 @@ import { Component, OnInit } from '@angular/core';
 export class HomePage implements OnInit {
 
   usuario = localStorage.getItem("datos")
-  
-  constructor() { }
+  @ViewChild('slideWithNav', { static: false }) slideWithNav: IonSlides;
+
+  sliderOne: any;
+
+  slideOptions = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    autoplay: true
+  };
+
+  constructor( private ruta : Router) { 
+     //Item object for Nature
+     this.sliderOne =
+     {
+       isBeginningSlide: true,
+       isEndSlide: false,
+       slidesItems: [
+         {
+           id: 100
+         },
+         {
+           id: 200
+         },
+         {
+           id: 300
+         },
+       ]
+     };
+  }
 
   ngOnInit() {
+  }
+
+  slideNext(object, slideView) {
+    slideView.slideNext(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });
+  }
+
+  //Move to previous slide
+  slidePrev(object, slideView) {
+    slideView.slidePrev(500).then(() => {
+      this.checkIfNavDisabled(object, slideView);
+    });;
+  }
+
+  //Method called when slide is changed by drag or navigation
+  SlideDidChange(object, slideView) {
+    this.checkIfNavDisabled(object, slideView);
+  }
+
+  //Call methods to check if slide is first or last to enable disbale navigation  
+  checkIfNavDisabled(object, slideView) {
+    this.checkisBeginning(object, slideView);
+    this.checkisEnd(object, slideView);
+  }
+
+  checkisBeginning(object, slideView) {
+    slideView.isBeginning().then((istrue) => {
+      object.isBeginningSlide = istrue;
+    });
+  }
+  checkisEnd(object, slideView) {
+    slideView.isEnd().then((istrue) => {
+      object.isEndSlide = istrue;
+    });
+  }
+  redireccionarChaquetas(){
+    this.ruta.navigate(['/chaquetas']);
+
   }
 }
